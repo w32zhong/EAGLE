4a50883b0428f8cf158517e2a758c014a472e185
w32zhong <clock126@126.com>
2024-09-15 22:51:18 -0400

    introduce pandas fire.

--- application/test_awq.py
+++ application/test_awq.py
@@ -22,7 +22,27 @@ from awq.modules.linear import (
 import sys
 sys.path.insert(0, '.')
 from model.ea_model import EaModel
-use_original = False
+
+
+def test_vanilla(model_path='NousResearch/Llama-2-7b-chat-hf', save_dir='save'):
+    prompt = '[INST] tell me something interesting about the solar eclipse in April 2024. [/INST]'
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    if save_dir is None: # inference
+        model = AutoAWQForCausalLM.from_quantized('save/', fuse_layers=False)
+        inputs = tokenizer([prompt], return_tensors="pt").to('cuda:0')
+        model.eval()
+        print(model)
+        streamer = TextStreamer(tokenizer)
+        with torch.no_grad():
+            output = model.generate(**inputs, streamer=streamer, max_length=128)
+    else:
+        quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+        model = AutoAWQForCausalLM.from_pretrained(
+            model_path, device_map="auto", use_cache=False
+        )
+        model.quantize(tokenizer, quant_config=quant_config, apply_clip=False)
+        model.save_quantized(save_dir)
+
 
 class EagleAWQForCausalLM(BaseAWQForCausalLM):
     layer_type = "LlamaDecoderLayer"
@@ -48,27 +68,16 @@ class EagleAWQForCausalLM(BaseAWQForCausalLM):
             for subkey, submod in layer.named_modules():
                 name = submod.__class__.__name__.lower()
                 if 'wqlinear' in name or submod in layer_prev_ops:
-                    to_save.append((subkey, submod, name))
-            for subkey, submod, name in to_save:
-                sd = submod.state_dict()
-                for state_key in sd:
-                    key = f'{layer_path}.{subkey}.{state_key}'
-                    Q_state_dict[key] = sd[state_key]
+                    sd = submod.state_dict()
+                    for state_key in sd:
+                        key = f'{layer_path}.{subkey}.{state_key}'
+                        Q_state_dict[key] = sd[state_key]
         torch.save(Q_state_dict, save_pth)
 
     @staticmethod
     def get_model_layers(model):
         layers = model.base_model.model.layers + model.ea_layer.layers
         return layers
-
-    @staticmethod
-    def get_act_for_scaling(module):
-        return dict(is_scalable=False)
-
-    @staticmethod
-    def move_embed(model, device: str):
-        model.base_model.model.embed_tokens.to(device)
-        model.ea_layer.embed_tokens.to(device)
 
     @staticmethod
     def get_layers_for_scaling(module, input_feat, module_kwargs):
@@ -206,26 +215,16 @@ def create_calib_files(tokenizer, awq_model, ea_model, save_dir):
         clear_memory()
 
 
-def quantize(save_dir='save', create_calib=False):
-    tokenizer, ea_model, awq_model = EagleAWQForCausalLM.from_pretrained(
-        base_model_path='NousResearch/Llama-2-7b-chat-hf',
-        ea_model_path='yuhuili/EAGLE-llama2-chat-7B',
-        torch_dtype=torch.float16,
-        device_map=('auto' if not use_original else None)
-    )
-
-    quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
-
-    if use_original:
-        awq_model.quantize(tokenizer, quant_config=quant_config)
-        quit()
-
-    if create_calib:
-        create_calib_files(tokenizer, awq_model, ea_model, './save/calib')
-        quit()
-
+def AWQ_quantize(slice_layers, quant_config, awq_model):
+    group_size = quant_config['q_group_size']
+    fname = f'awq-layers({slice_layers.start}-{slice_layers.stop}-{slice_layers.step})-g{group_size}'
+    save_pth = f'save/{fname}.pth'
+    os.makedirs('save', exist_ok=True)
+    if os.path.exists(save_pth):
+        return save_pth
     all_layer_paths = [f'model.base_model.model.layers.{l}' for l in range(32)] + ['model.ea_layer.layers.0']
-    for i, layer_path in enumerate(all_layer_paths[:]):
+    print('Targeting layers:', all_layer_paths[slice_layers])
+    for i, layer_path in enumerate(all_layer_paths[slice_layers]):
         print(f'Quantizing {i}-th layer:', layer_path)
         with open(f'./save/calib/{layer_path}.pkl', 'rb') as fh:
             input_feat = pickle.load(fh)
@@ -237,62 +236,31 @@ def quantize(save_dir='save', create_calib=False):
             feat = feat[:,:-extra,:].reshape(-1, 512, dim)
             feat = feat[:batch, ...]
             input_feat[key] = feat
-            print(key, feat.shape)
+            #print(key, feat.shape)
         clear_memory()
         layer = awq_model.get_submodule(layer_path)
         awq_model.quantize_layer(layer, input_feat, quant_config)
         clear_memory()
 
-    os.makedirs(save_dir, exist_ok=True)
-    awq_model.save_quantized(all_layer_paths, f'{save_dir}/save.pth')
+    awq_model.save_quantized(all_layer_paths[slice_layers], save_pth)
+    return save_pth
 
 
-def test_vanilla(model_path='NousResearch/Llama-2-7b-chat-hf', save_dir='save'):
-    prompt = '[INST] tell me something interesting about the solar eclipse in April 2024. [/INST]'
-    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    if save_dir is None: # inference
-        model = AutoAWQForCausalLM.from_quantized('save/', fuse_layers=False)
-        inputs = tokenizer([prompt], return_tensors="pt").to('cuda:0')
-        model.eval()
-        print(model)
-        streamer = TextStreamer(tokenizer)
-        with torch.no_grad():
-            output = model.generate(**inputs, streamer=streamer, max_length=128)
-    else:
-        quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
-        model = AutoAWQForCausalLM.from_pretrained(
-            model_path, device_map="auto", use_cache=False
-        )
-        model.quantize(tokenizer, quant_config=quant_config, apply_clip=False)
-        model.save_quantized(save_dir)
+def test(comment, quantize_top_layer=False, load_in_8bit=False, load_in_4bit=False,
+        awq_group_size=-1, awq_layers=None, awq_kernel="GEMM", results=None):
 
-
-def load_and_test(mode, pth_path='save/save.pth'):
-    if mode == 'fp16':
-        kwargs = dict(quantize_top_layer=False, load_in_4bit=False)
-    elif mode == 'int8':
-        kwargs = dict(quantize_top_layer=True, load_in_8bit=True)
-    elif mode == 'nf4':
-        kwargs = dict(quantize_top_layer=True, load_in_4bit=True)
-    elif mode == 'nf4-baseonly':
-        kwargs = dict(quantize_top_layer=False, load_in_4bit=True)
-    elif mode == 'nf4-toponly':
-        kwargs = dict(quantize_top_layer=True)
-    elif mode == 'nf4-awq':
-        kwargs = dict(quantize_top_layer=False, load_in_4bit=True)
-    elif mode == 'awq':
-        kwargs = dict()
-    else:
-        raise ValueError
     tokenizer, ea_model, awq_model = EagleAWQForCausalLM.from_pretrained(
         base_model_path='NousResearch/Llama-2-7b-chat-hf',
         ea_model_path='yuhuili/EAGLE-llama2-chat-7B',
         torch_dtype=torch.float16,
-        device_map=('auto' if not use_original else None),
+        device_map='auto',
         ########
-        **kwargs
+        quantize_top_layer=quantize_top_layer,
+        load_in_8bit=load_in_8bit,
+        load_in_4bit=load_in_4bit
     )
     tokenizer = ea_model.tokenizer
+    #create_calib_files(tokenizer, awq_model, ea_model, './save/calib')
 
     def parent(key):
         path_fields = key.split('.')
@@ -300,10 +268,29 @@ def load_and_test(mode, pth_path='save/save.pth'):
         child_key = path_fields[-1]
         return parent_key, child_key
 
-    if 'awq' in mode:
-        quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+    if awq_layers:
+        quant_config = {
+            "zero_point": True,
+            "q_group_size": awq_group_size,
+            "w_bit": 4,
+            "version": awq_kernel
+        }
+        awq_layers = eval(awq_layers)
+        Q_state_dict = torch.load(AWQ_quantize(awq_layers, quant_config, awq_model))
+        del tokenizer, ea_model, awq_model
+        clear_memory()
+        clear_memory()
+        breakpoint()
+        tokenizer, ea_model, awq_model = EagleAWQForCausalLM.from_pretrained(
+            base_model_path='NousResearch/Llama-2-7b-chat-hf',
+            ea_model_path='yuhuili/EAGLE-llama2-chat-7B',
+            torch_dtype=torch.float16,
+            device_map='auto',
+            quantize_top_layer=quantize_top_layer,
+            load_in_8bit=load_in_8bit,
+            load_in_4bit=load_in_4bit
+        )
         quant_config = AwqConfig.from_dict(quant_config)
-        Q_state_dict = torch.load(pth_path)
         Q_mod_keys = set([parent(key)[0] for key in Q_state_dict.keys()])
         to_be_replaced = []
         for key, module in awq_model.named_modules():
@@ -320,14 +307,11 @@ def load_and_test(mode, pth_path='save/save.pth'):
             elif version == "gemv_fast":
                 q_linear_module = WQLinear_GEMVFast
             else:
-                q_linear_module = None
+                raise NotImplemented
 
             q_linear = q_linear_module.from_linear(
                 module, quant_config.w_bit, quant_config.q_group_size, True
             )
-            #for subkey in q_linear.state_dict():
-            #    src = Q_state_dict[key + '.' + subkey]
-            #    setattr(q_linear, subkey, src)
             q_linear.to(next(module.parameters()).device)
 
             parent_key, child_key = parent(key)
@@ -344,8 +328,9 @@ def load_and_test(mode, pth_path='save/save.pth'):
             q_linear.weight = q_linear.qweight # proxy for compability
             setattr(p_module, child_key, q_linear)
 
+        awq_model.load_state_dict(Q_state_dict, strict=False)
+
     print(ea_model)
-    awq_model.load_state_dict(Q_state_dict, strict=False)
     with torch.no_grad():
         prompt = '[INST] tell me something interesting about the solar eclipse in April 2024. [/INST]'
         input_ids = tokenizer([prompt], return_tensors="pt").input_ids
@@ -364,21 +349,45 @@ def load_and_test(mode, pth_path='save/save.pth'):
         time_delta = time.time() - start_time
     speed = cnt_tokens / time_delta
     print('e2e speed:', time_delta, cnt_tokens, speed)
+    results['time_delta'] = time_delta
+    results['cnt_tokens'] = cnt_tokens
+    results['speed'] = speed
 
 
 if __name__ == '__main__':
-                                        # vinilla_speed=15
-    #load_and_test('fp16')              # speed=42.5
-    #load_and_test('int8')              # speed=9.6
-    #load_and_test('nf4')               # speed=5.3
-
-    #load_and_test('nf4-baseonly')      # speed=8.7
-    #load_and_test('nf4-toponly')       # speed=19.8   ***
-    #load_and_test('nf4-awq')           # speed=6.6
-    #load_and_test('fp16-awq', 'save/model.ea_layer.layers.0.pth')          # speed=31.2   ***
-
-    #quantize(create_calib=False)
-    load_and_test('awq')
-
-    #test_vanilla()
-    #test_vanilla(save_dir=None)
+    import os
+    import argparse
+    import pandas as pd
+    import multiprocessing, threading
+    from colorama import Fore, Back, Style
+    parser = argparse.ArgumentParser(description='Pandas Fire ArgumentParser')
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    manager = multiprocessing.Manager()
+    df_params = pd.read_csv('params-awq.tsv', sep='\t', header=0)
+    df_params = df_params.replace({float('nan'): None})
+    df_results = []
+    for params in df_params.to_dict(orient='records'):
+        print(Fore.RED, Back.YELLOW, params, Style.RESET_ALL)
+        try:
+            params['results'] = manager.dict()
+            os.environ["TOKENIZERS_PARALLELISM"] = "true"
+            if args.debug:
+                process = threading.Thread(target=test, kwargs=params)
+            else:
+                process = multiprocessing.Process(target=test, kwargs=params)
+            process.start()
+            process.join()
+        except:
+            if args.debug:
+                pass
+            else:
+                process.terminate()
+            break
+        results = dict(params['results'])
+        print(Fore.RED, Back.YELLOW, results, Style.RESET_ALL, end='\n\n')
+        df_results.append(results)
+    df_results = pd.DataFrame(df_results)
+    df_output = df_params.join(df_results)
+    print(df_output)
+    df_output.to_csv('output-awq.tsv', sep='\t', index=False)
