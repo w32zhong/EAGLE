68e7c1800d3e3fda9a1704b2f3aa040f49349bfe
w32zhong <clock126@126.com>
2025-03-02 23:09:11 -0500

    update.

--- model/ea_model.py
+++ model/ea_model.py
@@ -35,8 +35,9 @@ def my_ckpt_convert(state_dict, base_model):
             base_model_param = base_model.get_parameter(key)
             torch.isclose(base_model_param, val.to(base_model_param.device))
             continue
-        elif key.startswith('speculative_decoder.input_layernorm'):
+        elif 'input_layernorm' in key:
             print('Warning: EAGLE does not have input_layernorm!')
+            print(key, val)
             continue
 
         key = key.replace('model.', '')
