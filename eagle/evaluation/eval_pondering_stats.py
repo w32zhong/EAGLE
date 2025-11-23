997f6c483fd8a08ffd5720bc76afcdfef9518b30
w32zhong <clock126@126.com>
2025-11-23 05:00:59 +0000

    eval_pondering_stats.py: visualize more info.

--- eagle/evaluation/eval_pondering_stats.py
+++ eagle/evaluation/eval_pondering_stats.py
@@ -2,12 +2,14 @@ import json
 import fire
 import numpy as np
 import matplotlib.pyplot as plt
+from colorama import Fore, Style
 
 
-def main(json_file='pondering_stats.json'):
+def main(json_file='pondering_stats.json', threshold=0.5):
     with open(json_file) as fh:
         j = json.load(fh)
     print(j.keys())
+    max_length = len(j.keys()) - 1
     prev_survive_probs, last_survive_probs = [], []
     prev_exit_probs, last_exit_probs = [], []
     for iter_num, accept_length in enumerate(j['a']):
@@ -17,13 +19,19 @@ def main(json_file='pondering_stats.json'):
             survive *= (1 - e_i)
             prev_exit_probs.append(e_i)
             prev_survive_probs.append(survive)
-            print(round(survive, 2), end=' ')
+            color = Fore.RED if e_i > threshold else Style.RESET_ALL
+            print(f'{color}{round(e_i, 2)}{Style.RESET_ALL}', end=' ')
+        print(Fore.YELLOW + '| ', end=Style.RESET_ALL)
         if accept_length > 0:
             prev_exit_probs.pop()
             prev_survive_probs.pop()
             last_exit_probs.append(e_i)
             last_survive_probs.append(survive)
-            print()
+        for i in range(accept_length, max_length):
+            e_i = j[f'e{i}'][iter_num]
+            color = Fore.RED if e_i > threshold else Style.RESET_ALL
+            print(f'{color}{round(e_i, 2)}{Style.RESET_ALL}', end=' ')
+        print()
 
     fig, ax = plt.subplots(1, 4, figsize=(9, 2))
     ax[0].hist(prev_exit_probs, bins=10)
