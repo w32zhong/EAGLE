83a8700dd0c91034ed0034b272a474206a09765f
w32zhong <clock126@126.com>
2025-12-06 18:47:56 +0000

    hardware issue?

--- eagle/evaluation/gen_ea_answer_llama3chat.py
+++ eagle/evaluation/gen_ea_answer_llama3chat.py
@@ -239,11 +239,11 @@ def get_model_answers(
                 )
                 input_ids = tokenizer([prompt], add_special_tokens=False, ).input_ids
 
-                import pickle
-                with open('/home/test_input.pkl', 'rb') as file:
-                    ddd = pickle.load(file)
-                    inject_input_ids = ddd['input_ids'].tolist()
-                    input_ids = [inject_input_ids[0][:191]]
+                #import pickle
+                #with open('/home/test_input.pkl', 'rb') as file:
+                #    ddd = pickle.load(file)
+                #    inject_input_ids = ddd['input_ids'].tolist()
+                #    input_ids = [inject_input_ids[0][:191]]
 
                 # try:
                 torch.cuda.synchronize()
