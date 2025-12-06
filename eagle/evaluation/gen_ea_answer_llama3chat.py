a68f79159c19f6dbc6a752039061cd234dcaebcc
w32zhong <clock126@126.com>
2025-12-06 23:39:26 +0000

    2-layer draft is sensitive to precision.

--- eagle/evaluation/gen_ea_answer_llama3chat.py
+++ eagle/evaluation/gen_ea_answer_llama3chat.py
@@ -111,8 +111,7 @@ def get_model_answers(
         pondering_threshold=args.pondering_threshold,
         pondering_options=args.pondering_options,
         # extra kwargs:
-        torch_dtype=torch.float16,
-        low_cpu_mem_usage=True,
+        torch_dtype=torch.bfloat16,
         # load_in_8bit=True,
         device_map="auto",
         use_eagle3=args.use_eagle3,
--- eagle/model/modeling_llama_kv.py
+++ eagle/model/modeling_llama_kv.py
@@ -1016,8 +1016,8 @@ class LlamaModel(LlamaPreTrainedModel):
         if input_shape[-1] > 1:
             combined_attention_mask = _make_causal_mask(
                 input_shape,
-                # inputs_embeds.dtype,
-                torch.float32,  # [MODIFIED] force to cast to float32
+                inputs_embeds.dtype,
+                #torch.float32,  # [MODIFIED] force to cast to float32
                 device=inputs_embeds.device,
                 past_key_values_length=past_key_values_length,
             )
